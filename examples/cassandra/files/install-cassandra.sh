e8d49ccea5d90467d16b3aba178325c565c52699
Andrey Konstantinov <avkonst@gmail.com>
2017-06-09 00:08:57 +1200

    Refactored cassandra image to make it runable by clusterlite.

--- examples/cassandra/files/install-cassandra.sh
+++ examples/cassandra/files/install-cassandra.sh
@@ -8,13 +8,15 @@ set -e
 
 echo "Installing cassandra"
 
-VERSION=3.10
+apt-get update
+apt-get -qq -y install --no-install-recommends procps libjemalloc1
+rm -rf /var/lib/apt/lists/* ~/.bashrc
 
+VERSION=3.10
 #MIRROR=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
 MIRROR="http://apache.mirror.amaze.com.au/"
-
-wget -q -O - $MIRROR/cassandra/$VERSION/apache-cassandra-$VERSION-bin.tar.gz | tar -xzf - -C /opt
-mv /opt/apache-cassandra-$VERSION /opt/cassandra
+wget -q -O - ${MIRROR}/cassandra/${VERSION}/apache-cassandra-${VERSION}-bin.tar.gz | tar -xzf - -C /opt
+mv /opt/apache-cassandra-${VERSION} /opt/cassandra
 mv /cassandra.yaml /opt/cassandra/conf
 
 echo "Installed cassandra"
