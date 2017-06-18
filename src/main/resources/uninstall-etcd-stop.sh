3025e2c2cfca32193cb229158385c2abf4cf540b
Andrey Konstantinov <avkonst@gmail.com>
2017-06-19 00:53:07 +1200

    minor bug fixes

--- src/main/resources/uninstall-etcd-stop.sh
+++ src/main/resources/uninstall-etcd-stop.sh
@@ -1,3 +1,4 @@
+
     echo "__LOG__ stopping etcd server"
     docker exec -it clusterlite-etcd /run-etcd-remove.sh || \
         echo "__LOG__ warning: failure to detach etcd server"
@@ -7,3 +8,4 @@
         echo "__LOG__ warning: failure to remove etcd container"
     rm -Rf __VOLUME__/clusterlite-etcd || \
         echo "__LOG__ warning: failure to remove etcd data"
+
--- src/main/resources/uninstall.sh
+++ src/main/resources/uninstall.sh
@@ -20,16 +20,15 @@ uninstall() {
         echo "__LOG__ warning: failure to stop clusterlite-proxy container"
     docker rm clusterlite-proxy || \
         echo "__LOG__ warning: failure to remove clusterlite-proxy container"
-
 __ETCD_STOP_PART__
     echo "__LOG__ uninstalling weave network"
     # see https://www.weave.works/docs/net/latest/ipam/stop-remove-peers-ipam/
     weave reset || echo "__LOG__ warning: failure to reset weave network"
 
-    rm -Rf __VOLUME__ || echo "__LOG__ warning: some data has not been removed"
-    echo "__LOG__ done"
-
     echo "__LOG__ uninstalling data directory"
+    rm -Rf __VOLUME__ || echo "__LOG__ warning: some data has not been removed"
     rm -Rf /var/lib/clusterlite || echo "__LOG__ warning: some data has not been removed"
+
+    echo "__LOG__ done"
 }
 uninstall
