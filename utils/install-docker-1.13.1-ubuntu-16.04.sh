b53f4e0d2995a9819b830ae2ff791e81f3f38a6a
Andrey Konstantinov <avkonst@gmail.com>
2017-06-22 18:12:23 +1200

    minor improvement

--- utils/install-docker-1.13.1-ubuntu-16.04.sh
+++ utils/install-docker-1.13.1-ubuntu-16.04.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/env bash
 
-
-if [[ $(lsb_release -a | grep xenial | wc -l) == "1" ]]
+if [[ $(which docker | wc -l) == "0" ]]
 then
-    # ubuntu supports automated installation
-    (>&2 echo "$log installing docker")
-    apt-get -y update || (>&2 echo "apt-get update failed, are proxy settings correct?" && exit 1)
-    apt-get -qq -y install --no-install-recommends curl
+    if [[ $(lsb_release -a | grep xenial | wc -l) == "1" ]]
+    then
+        # ubuntu supports automated installation
+        (>&2 echo "$log installing docker")
+        apt-get -y update || (>&2 echo "apt-get update failed, are proxy settings correct?" && exit 1)
+        apt-get -qq -y install --no-install-recommends curl
+    else
+        (>&2 echo "failure: required: Ubuntu 16.04, found: $(lsb_release -a)")
+        exit 1
+    fi
+
+    # Run the installation script to get the latest docker version.
+    # This is disabled in favor of controlled migration to latest docker versions
+    # curl -sSL https://get.docker.com/ | sh
+
+    # Use specific version for installation
+    apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+    mkdir -p /etc/apt/sources.list.d || echo ""
+    echo deb https://apt.dockerproject.org/repo ubuntu-xenial main > /etc/apt/sources.list.d/docker.list
+    apt-get update
+    apt-get -qq -y install --no-install-recommends docker-engine=1.13.1-0~ubuntu-xenial
+
+    docker --version
+
+    # Verify that Docker Engine is installed correctly:
+    docker run hello-world
 else
-    (>&2 echo "failure: required: Ubuntu 16.04, found: $(lsb_release -a)")
-    exit 1
+    (>&2 echo "$log docker is already installed")
 fi
-
-# Run the installation script to get the latest docker version.
-# This is disabled in favor of controlled migration to latest docker versions
-# curl -sSL https://get.docker.com/ | sh
-
-# Use specific version for installation
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-mkdir -p /etc/apt/sources.list.d || echo ""
-echo deb https://apt.dockerproject.org/repo ubuntu-xenial main > /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get -qq -y install --no-install-recommends docker-engine=1.13.1-0~ubuntu-xenial
-
-docker --version
-
-# Verify that Docker Engine is installed correctly:
-docker run hello-world
