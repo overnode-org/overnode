c8602f8fc735eb74366a3691f2ec81f7ee9306e5
Andrey Konstantinov <avkonst@gmail.com>
2017-06-25 04:37:27 +1200

    minor visual improvement

--- clusterlite.sh
+++ clusterlite.sh
@@ -50,8 +50,8 @@ usage_no_exit() {
     version                                => Print version information.
     -------------------------------------------------------------------------------------------------------------
     install                                => Install clusterlite node on the current host and join the cluster.
-      --token <cluster-wide-token>          | Token should be the same for all nodes joining the cluster.
-      --seeds <host1,host2,...>             | 3-5 seeds are recommended for high-availability and reliability.
+      --token <cluster-wide-token>         -> Token should be the same for all nodes joining the cluster.
+      --seeds <host1,host2,...>            -> 3-5 seeds are recommended for high-availability and reliability.
                                             | Hosts should be private IP addresses or valid DNS host names.
                                             | If 1 host is planned initially, initialize as the following:
                                             |   host1$ clusterlite install --seeds host1
@@ -65,9 +65,9 @@ usage_no_exit() {
                                             | If a host is joining as non seed host, initialize as the following:
                                             |   host4$ clusterlite install --seeds host1,host2,host3
                                             | WARNING: seeds order should be the same on all joining hosts!
-      [--volume /var/lib/clusterlite]       | Directory where stateful services will persist data.
-      [--public-address]                    | Public IP address of the host, if exists and requires exposure.
-      [--placement default]                 | Role allocation for a node. A node schedules services
+      [--volume /var/lib/clusterlite]      -> Directory where stateful services will persist data.
+      [--public-address]                   -> Public IP address of the host, if exists and requires exposure.
+      [--placement default]                -> Role allocation for a node. A node schedules services
                                             | according to the matching placement
                                             | defined in the configuration file set via 'apply' action.
     uninstall                              => Destroy processes/containers, leave the cluster and remove data.
@@ -75,27 +75,27 @@ usage_no_exit() {
     info                                   => Show cluster-wide information, like IDs of nodes.
     -------------------------------------------------------------------------------------------------------------
     login                                  => Provide credentials to download images from private repositories.
-      --username <username>                 | Docker registry username.
-      --password <password>                 | Docker registry password.
-      [--registry registry.hub.docker.com]  | Address of docker registry to login to.
+      --username <username>                -> Docker registry username.
+      --password <password>                -> Docker registry password.
+      [--registry registry.hub.docker.com] -> Address of docker registry to login to.
                                             | If you have got multiple different registries,
                                             | execute 'login' action multiple times.
                                             | Credentials can be also different for different registries.
     logout                                 => Removes credentials for a registry
-      [--registry registry.hub.docker.com]  | Address of docker registry to logout from.
+      [--registry registry.hub.docker.com] -> Address of docker registry to logout from.
     -------------------------------------------------------------------------------------------------------------
     plan                                   => Review what current or new configuration requires to apply.
-      [--config /path/to/yaml/file]         | The same as for 'apply' action.
+      [--config /path/to/yaml/file]        -> The same as for 'apply' action.
     apply                                  => Apply current or new configuration and provision services.
-      [--config /path/to/yaml/file]         | Configuration file for the cluster, which defines
+      [--config /path/to/yaml/file]        -> Configuration file for the cluster, which defines
                                             | what containers to create and where to launch them.
                                             | If it is not defined, the latest applied is used.
     show                                   => Show current status of created containers / services.
     destroy                                => Terminate and destroy all containers / services in the cluster.
     -------------------------------------------------------------------------------------------------------------
     docker                                 => Run docker command against one or multiple nodes of the cluster.
-      [--nodes 1,2,..]                      | Comma separated list of IDs of nodes. If absent, applies to all.
-      <docker-command> [docker-options]     | Valid docker command and options. For example:
+      [--nodes 1,2,..]                     -> Comma separated list of IDs of nodes. If absent, applies to all.
+      <docker-command> [docker-options]    -> Valid docker command and options. For example:
                                             | - List running containers on node 1:
                                             |   host1$ clusterlite docker --nodes 1 ps
                                             | - Print logs for my-service container running on nodes 1 and 2:
@@ -106,7 +106,7 @@ usage_no_exit() {
     expose                                 => Allow the current host to access the network of the cluster.
     hide                                   => Disallow the current host to access the network of the cluster.
     lookup                                 => Execute DNS lookup against the internal DNS service of the cluster.
-      <name-to-lookup>                      | Service name or container name to lookup.
+      <name-to-lookup>                     -> Service name or container name to lookup.
     -------------------------------------------------------------------------------------------------------------
 EOF
 }
