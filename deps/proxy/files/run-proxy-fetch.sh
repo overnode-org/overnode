eace25daf32b05b022c4088af3de32969b22a863
Andrey Konstantinov <avkonst@gmail.com>
2017-07-04 00:03:04 +1200

    file fetch developed

--- deps/proxy/files/run-proxy-fetch.sh
+++ deps/proxy/files/run-proxy-fetch.sh
@@ -6,20 +6,28 @@
 
 set -e
 
+error_and_exit() {
+    file_reference=$1
+    curl http://clusterlite-etcd:2379/v2/keys/files/${file_reference} >&2
+    echo "[clusterlite proxy-fetch] failure to fetch http://clusterlite-etcd:2379/v2/keys/files/${file_reference}" >&2
+    exit 1
+}
+
 while [ ! -z $1 ]; do
     service_name=$1
     shift
-    file_reference=$2
+    file_reference=$1
     shift
-    file_name=$3
+    file_name=$1
     shift
     echo "[clusterlite proxy-fetch] downloading ${file_reference} => ${service_name}/${file_name}"
 
     curl --fail -s http://clusterlite-etcd:2379/v2/keys/files/${file_reference} | jq -j -e ".node.value" > /data/${file_name}
-    [[ ${PIPESTATUS[0]} -eq "0" && $? -eq "0" ]] || \
-        (curl http://clusterlite-etcd:2379/v2/keys/files/${file_reference} >&2; \
-         echo "[clusterlite proxy-fetch] failure to fetch http://clusterlite-etcd:2379/v2/keys/files/${file_reference}" >&2; \
-         exit 1)
+    jq_code=$?
+
+    if [ ${jq_code} -ne "0" ];then
+        error_and_exit ${file_reference}
+    fi
 done
 
 echo "[clusterlite proxy-fetch] success: action completed"
