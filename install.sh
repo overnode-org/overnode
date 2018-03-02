82f9e84eec17edab6eaef3e9449f619448d98ec4
Andrey Konstantinov <avkonst@gmail.com>
2018-03-02 20:11:25 +1300

    reverting changes for automatically generated file.

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@
 
 set -e
 
-version_system=0.7.0
-wget --no-cache -O /usr/bin/cade https://raw.githubusercontent.com/cadeworks/cade/0.7.0/cade.sh
+version_system=0.6.17
+wget --no-cache -O /usr/bin/cade https://raw.githubusercontent.com/cadeworks/cade/0.6.17/cade.sh
 chmod a+x /usr/bin/cade
 
 cade --debug version || (echo "cade installation failed" && exit 1)
