46b59330cf7392c247861b3f00dadabe367110f3
Andrey Konstantinov <avkonst@gmail.com>
2020-04-09 04:05:04 +0000

    Release 0.8.3

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ function set_console_normal() {
 trap set_console_normal EXIT
 
 set_console_color 
+[ ! -f /tmp/overnode ] || rm /tmp/overnode
 wget --no-cache -O - https://raw.githubusercontent.com/avkonst/overnode/0.8.3/overnode.sh > /tmp/overnode
 chmod u+x /tmp/overnode
 set_console_normal
