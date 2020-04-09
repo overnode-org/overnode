9388f8dc9b70b23a05a0dc33e967ad82b62dfd1e
Andrey Konstantinov <avkonst@gmail.com>
2020-04-09 18:25:54 +1200

    Release 0.8.7

--- install.sh
+++ install.sh
@@ -2,34 +2,23 @@
 
 set -e
 
-yellow_c='\033[0;33m'
-gray_c='\033[1;30m'
-no_c='\033[0;37m' # white
-
-trap "echo "" >&2" EXIT
-
-echo "" >&2
 [ ! -f /tmp/overnode ] || rm /tmp/overnode
-wget --no-cache -O - https://raw.githubusercontent.com/avkonst/overnode/0.8.6/overnode.sh > /tmp/overnode
-chmod u+x /tmp/overnode
-echo "" >&2
+wget -q --no-cache -O - https://raw.githubusercontent.com/avkonst/overnode/0.8.7/overnode.sh > /tmp/overnode || (echo "*** overnode download failed" && exit 1)
+chmod a+x /tmp/overnode
 
-/tmp/overnode --debug version || (echo "overnode download failed" && exit 1)
+/tmp/overnode --debug version || (echo "*** overnode download corrupted" && exit 1)
 
 if [ ! -f /usr/bin/overnode ]
 then
-    /tmp/overnode install
-    mv /tmp/overnode /usr/bin/overnode || (echo "overnode install failed" && exit 1)
+    /tmp/overnode install || (echo "*** overnode install failed" && exit 1)
+    mv /tmp/overnode /usr/bin/overnode || (echo "*** overnode install failed" && exit 1)
 else
     if [ $# -eq 0 ]
     then
-        echo "" >&2
-        echo "/usr/bin/overnode file exists already"
-        echo "run 'overnode upgrade' instead"
-        echo "" >&2
+        /usr/bin/overnode install || (echo "*** overnode install failed" && exit 1)
     else
-        /tmp/overnode install --force
-        mv /tmp/overnode /usr/bin/overnode || (echo "overnode upgrade failed" && exit 1)
+        /tmp/overnode install --force || (echo "*** overnode upgrade failed" && exit 1)
+        mv /tmp/overnode /usr/bin/overnode || (echo "*** overnode upgrade failed" && exit 1)
     fi
 fi
 
