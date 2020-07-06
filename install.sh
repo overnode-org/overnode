f870d563bebd35e8e166c29e3d6497736b1ea48b
Andrey Konstantinov <avkonst@gmail.com>
2020-07-06 16:52:51 +1200

    Release 0.11.0

--- install.sh
+++ install.sh
@@ -3,21 +3,21 @@
 set -e
 
 [ ! -f /tmp/overnode ] || rm /tmp/overnode
-wget -q --no-cache -O - https://raw.githubusercontent.com/avkonst/overnode/0.10.2/overnode.sh > /tmp/overnode || (echo "*** overnode download failed" && exit 1)
+wget -q --no-cache -O - https://raw.githubusercontent.com/avkonst/overnode/0.11.0/overnode.sh > /tmp/overnode || (echo "*** overnode download failed" && exit 1)
 chmod a+x /tmp/overnode
 
 /tmp/overnode --debug version || (echo "*** overnode download corrupted" && exit 1)
 
 if [ ! -f /usr/bin/overnode ]
 then
-    /tmp/overnode install || (echo "*** overnode install failed" && exit 1)
+    /tmp/overnode --debug install || (echo "*** overnode install failed" && exit 1)
     mv /tmp/overnode /usr/bin/overnode || (echo "*** overnode install failed" && exit 1)
 else
     if [ $# -eq 0 ]
     then
-        /usr/bin/overnode install || (echo "*** overnode install failed" && exit 1)
+        /usr/bin/overnode --debug install || (echo "*** overnode install failed" && exit 1)
     else
-        /tmp/overnode install --force || (echo "*** overnode upgrade failed" && exit 1)
+        /tmp/overnode --debug install --force || (echo "*** overnode upgrade failed" && exit 1)
         mv /tmp/overnode /usr/bin/overnode || (echo "*** overnode upgrade failed" && exit 1)
     fi
 fi
