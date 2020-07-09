df4e2a58bef6971952e680063092dde99b9430ee
Andrey Konstantinov <avkonst@gmail.com>
2020-07-09 15:41:20 +1200

    0.11.11

--- overnode.sh
+++ overnode.sh
@@ -9,7 +9,7 @@ version_weave=2.6.5
 version_proxy=1.7.3.4-r0
 version_git=latest
 version_loki=master-de644b5 # ensure https://github.com/grafana/loki/issues/2313 is fixed before upgrading
-version_system=0.11.10
+version_system=0.11.11
 
 provider_proxy="alpine/socat"
 provider_git="alpine/git"
